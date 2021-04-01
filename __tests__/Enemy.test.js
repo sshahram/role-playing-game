09d331511c6726c61e097a8aad5fa3bb98f72121
sshahram <shirin.shahram23@gmail.com>
2021-03-31 18:32:04 -0700

    add Enemy constructor

---
+++ __tests__/Enemy.test.js
@@ -0,0 +1,15 @@
+const Enemy = require('../lib/Enemy.js');
+const Potion = require('../lib/Potion.js');
+
+jest.mock('../lib/Potion.js');
+
+test('creates an enemy object', () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.name).toBe('goblin');
+    expect(enemy.weapon).toBe('sword');
+    expect(enemy.health).toEqual(expect.any(Number));
+    expect(enemy.strength).toEqual(expect.any(Number));
+    expect(enemy.agility).toEqual(expect.any(Number));
+    expect(enemy.potion).toEqual(expect.any(Object));
+}); 
