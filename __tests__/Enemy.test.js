6e1b45a45cd8892cbb90500a24656992bf893ac6
sshahram <shirin.shahram23@gmail.com>
2021-03-31 19:29:07 -0700

    build enemy's methods

--- __tests__/Enemy.test.js
+++ __tests__/Enemy.test.js
@@ -13,3 +13,45 @@ test('creates an enemy object', () => {
     expect(enemy.agility).toEqual(expect.any(Number));
     expect(enemy.potion).toEqual(expect.any(Object));
 }); 
+
+test("get enemy's health value", () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.getHealth()).toEqual(expect.stringContaining(enemy.health.toString()));
+});
+
+test('checks if enemy is alive or not', () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.isAlive()).toBeTruthy();
+    enemy.health = 0;
+    expect(enemy.isAlive()).toBeFalsy();
+});
+
+test("subtracts from enemy's health", () => {
+    const enemy = new Enemy('goblin', 'sword');
+    const oldHealth = enemy.health;
+
+    enemy.reduceHealth(5);
+    expect(enemy.health).toBe(oldHealth - 5);
+
+    enemy.reduceHealth(99999);
+    expect(enemy.health).toBe(0);
+
+});
+
+// verifying a enemy's attack value is within range (enemy's strength - 5 <= enemy's attack value <= enemy's strength + 5)
+test("get enemy's attack value", () => {
+    const enemy = new Enemy('goblin', 'sword');
+    enemy.strength = 10;
+
+    expect(enemy.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(enemy.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test('gets a description of the enemy', () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.getDescription()).toEqual(expect.stringContaining('goblin'));
+    expect(enemy.getDescription()).toEqual(expect.stringContaining('sword'));
+});
